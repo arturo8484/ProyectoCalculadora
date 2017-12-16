c1b9dd476350510a135212a93b5312f7570207c5
Arturo Franco <34550874+arturo8484@users.noreply.github.com>
2017-12-15 22:16:46 -0400

    Proyecto calculadora

---
+++ FrmCalculadora/FrmCalculadora/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace FrmCalculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_num1 = new System.Windows.Forms.Button();
+            this.Btn_num2 = new System.Windows.Forms.Button();
+            this.Btn_num3 = new System.Windows.Forms.Button();
+            this.Btn_num4 = new System.Windows.Forms.Button();
+            this.Btn_num5 = new System.Windows.Forms.Button();
+            this.Btn_num6 = new System.Windows.Forms.Button();
+            this.Btn_num7 = new System.Windows.Forms.Button();
+            this.Btn_num8 = new System.Windows.Forms.Button();
+            this.Btn_num9 = new System.Windows.Forms.Button();
+            this.Btn_num0 = new System.Windows.Forms.Button();
+            this.Btn_suma = new System.Windows.Forms.Button();
+            this.Btn_resta = new System.Windows.Forms.Button();
+            this.Btn_multiplicacion = new System.Windows.Forms.Button();
+            this.Btn_Calcular = new System.Windows.Forms.Button();
+            this.btnCE = new System.Windows.Forms.Button();
+            this.Screen_Calculadora = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Btn_num1
+            // 
+            this.Btn_num1.BackColor = System.Drawing.Color.Black;
+            this.Btn_num1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_num1.ForeColor = System.Drawing.Color.White;
+            this.Btn_num1.Location = new System.Drawing.Point(16, 163);
+            this.Btn_num1.Name = "Btn_num1";
+            this.Btn_num1.Size = new System.Drawing.Size(54, 39);
+            this.Btn_num1.TabIndex = 0;
+            this.Btn_num1.Text = "1";
+            this.Btn_num1.UseVisualStyleBackColor = false;
+            this.Btn_num1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Btn_num2
+            // 
+            this.Btn_num2.BackColor = System.Drawing.Color.Black;
+            this.Btn_num2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_num2.ForeColor = System.Drawing.Color.White;
+            this.Btn_num2.Location = new System.Drawing.Point(76, 163);
+            this.Btn_num2.Name = "Btn_num2";
+            this.Btn_num2.Size = new System.Drawing.Size(54, 39);
+            this.Btn_num2.TabIndex = 1;
+            this.Btn_num2.Text = "2";
+            this.Btn_num2.UseVisualStyleBackColor = false;
+            this.Btn_num2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Btn_num3
+            // 
+            this.Btn_num3.BackColor = System.Drawing.Color.Black;
+            this.Btn_num3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_num3.ForeColor = System.Drawing.Color.White;
+            this.Btn_num3.Location = new System.Drawing.Point(136, 163);
+            this.Btn_num3.Name = "Btn_num3";
+            this.Btn_num3.Size = new System.Drawing.Size(54, 39);
+            this.Btn_num3.TabIndex = 2;
+            this.Btn_num3.Text = "3";
+            this.Btn_num3.UseVisualStyleBackColor = false;
+            this.Btn_num3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Btn_num4
+            // 
+            this.Btn_num4.BackColor = System.Drawing.Color.Black;
+            this.Btn_num4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_num4.ForeColor = System.Drawing.Color.White;
+            this.Btn_num4.Location = new System.Drawing.Point(16, 118);
+            this.Btn_num4.Name = "Btn_num4";
+            this.Btn_num4.Size = new System.Drawing.Size(54, 39);
+            this.Btn_num4.TabIndex = 3;
+            this.Btn_num4.Text = "4";
+            this.Btn_num4.UseVisualStyleBackColor = false;
+            this.Btn_num4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Btn_num5
+            // 
+            this.Btn_num5.BackColor = System.Drawing.Color.Black;
+            this.Btn_num5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_num5.ForeColor = System.Drawing.Color.White;
+            this.Btn_num5.Location = new System.Drawing.Point(76, 118);
+            this.Btn_num5.Name = "Btn_num5";
+            this.Btn_num5.Size = new System.Drawing.Size(54, 39);
+            this.Btn_num5.TabIndex = 4;
+            this.Btn_num5.Text = "5";
+            this.Btn_num5.UseVisualStyleBackColor = false;
+            this.Btn_num5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Btn_num6
+            // 
+            this.Btn_num6.BackColor = System.Drawing.Color.Black;
+            this.Btn_num6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_num6.ForeColor = System.Drawing.Color.White;
+            this.Btn_num6.Location = new System.Drawing.Point(136, 118);
+            this.Btn_num6.Name = "Btn_num6";
+            this.Btn_num6.Size = new System.Drawing.Size(54, 39);
+            this.Btn_num6.TabIndex = 5;
+            this.Btn_num6.Text = "6";
+            this.Btn_num6.UseVisualStyleBackColor = false;
+            this.Btn_num6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Btn_num7
+            // 
+            this.Btn_num7.BackColor = System.Drawing.Color.Black;
+            this.Btn_num7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_num7.ForeColor = System.Drawing.Color.White;
+            this.Btn_num7.Location = new System.Drawing.Point(16, 73);
+            this.Btn_num7.Name = "Btn_num7";
+            this.Btn_num7.Size = new System.Drawing.Size(54, 39);
+            this.Btn_num7.TabIndex = 6;
+            this.Btn_num7.Text = "7";
+            this.Btn_num7.UseVisualStyleBackColor = false;
+            this.Btn_num7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Btn_num8
+            // 
+            this.Btn_num8.BackColor = System.Drawing.Color.Black;
+            this.Btn_num8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_num8.ForeColor = System.Drawing.Color.White;
+            this.Btn_num8.Location = new System.Drawing.Point(76, 73);
+            this.Btn_num8.Name = "Btn_num8";
+            this.Btn_num8.Size = new System.Drawing.Size(54, 39);
+            this.Btn_num8.TabIndex = 7;
+            this.Btn_num8.Text = "8";
+            this.Btn_num8.UseVisualStyleBackColor = false;
+            this.Btn_num8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Btn_num9
+            // 
+            this.Btn_num9.BackColor = System.Drawing.Color.Black;
+            this.Btn_num9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_num9.ForeColor = System.Drawing.Color.White;
+            this.Btn_num9.Location = new System.Drawing.Point(136, 73);
+            this.Btn_num9.Name = "Btn_num9";
+            this.Btn_num9.Size = new System.Drawing.Size(54, 39);
+            this.Btn_num9.TabIndex = 8;
+            this.Btn_num9.Text = "9";
+            this.Btn_num9.UseVisualStyleBackColor = false;
+            this.Btn_num9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Btn_num0
+            // 
+            this.Btn_num0.BackColor = System.Drawing.Color.Black;
+            this.Btn_num0.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_num0.ForeColor = System.Drawing.Color.White;
+            this.Btn_num0.Location = new System.Drawing.Point(16, 208);
+            this.Btn_num0.Name = "Btn_num0";
+            this.Btn_num0.Size = new System.Drawing.Size(54, 39);
+            this.Btn_num0.TabIndex = 9;
+            this.Btn_num0.Text = "0";
+            this.Btn_num0.UseVisualStyleBackColor = false;
+            this.Btn_num0.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Btn_suma
+            // 
+            this.Btn_suma.BackColor = System.Drawing.Color.DarkRed;
+            this.Btn_suma.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_suma.Location = new System.Drawing.Point(196, 114);
+            this.Btn_suma.Name = "Btn_suma";
+            this.Btn_suma.Size = new System.Drawing.Size(40, 42);
+            this.Btn_suma.TabIndex = 11;
+            this.Btn_suma.Text = "+";
+            this.Btn_suma.UseVisualStyleBackColor = false;
+            this.Btn_suma.Click += new System.EventHandler(this.Btn_suma_Click);
+            // 
+            // Btn_resta
+            // 
+            this.Btn_resta.BackColor = System.Drawing.Color.DarkRed;
+            this.Btn_resta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_resta.Location = new System.Drawing.Point(196, 159);
+            this.Btn_resta.Name = "Btn_resta";
+            this.Btn_resta.Size = new System.Drawing.Size(40, 42);
+            this.Btn_resta.TabIndex = 12;
+            this.Btn_resta.Text = "-";
+            this.Btn_resta.UseVisualStyleBackColor = false;
+            this.Btn_resta.Click += new System.EventHandler(this.Btn_resta_Click);
+            // 
+            // Btn_multiplicacion
+            // 
+            this.Btn_multiplicacion.BackColor = System.Drawing.Color.DarkRed;
+            this.Btn_multiplicacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_multiplicacion.Location = new System.Drawing.Point(196, 205);
+            this.Btn_multiplicacion.Name = "Btn_multiplicacion";
+            this.Btn_multiplicacion.Size = new System.Drawing.Size(40, 42);
+            this.Btn_multiplicacion.TabIndex = 13;
+            this.Btn_multiplicacion.Text = "x";
+            this.Btn_multiplicacion.UseVisualStyleBackColor = false;
+            this.Btn_multiplicacion.Click += new System.EventHandler(this.Btn_multiplicacion_Click);
+            // 
+            // Btn_Calcular
+            // 
+            this.Btn_Calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Calcular.Location = new System.Drawing.Point(76, 208);
+            this.Btn_Calcular.Name = "Btn_Calcular";
+            this.Btn_Calcular.Size = new System.Drawing.Size(114, 39);
+            this.Btn_Calcular.TabIndex = 14;
+            this.Btn_Calcular.Text = "=";
+            this.Btn_Calcular.UseVisualStyleBackColor = true;
+            this.Btn_Calcular.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // btnCE
+            // 
+            this.btnCE.BackColor = System.Drawing.Color.DarkRed;
+            this.btnCE.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCE.Location = new System.Drawing.Point(196, 73);
+            this.btnCE.Name = "btnCE";
+            this.btnCE.Size = new System.Drawing.Size(40, 39);
+            this.btnCE.TabIndex = 15;
+            this.btnCE.Text = "CE";
+            this.btnCE.UseVisualStyleBackColor = false;
+            this.btnCE.Click += new System.EventHandler(this.btnCE_Click);
+            // 
+            // Screen_Calculadora
+            // 
+            this.Screen_Calculadora.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.Screen_Calculadora.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Screen_Calculadora.ForeColor = System.Drawing.Color.SpringGreen;
+            this.Screen_Calculadora.Location = new System.Drawing.Point(16, 23);
+            this.Screen_Calculadora.Name = "Screen_Calculadora";
+            this.Screen_Calculadora.Size = new System.Drawing.Size(220, 45);
+            this.Screen_Calculadora.TabIndex = 16;
+            this.Screen_Calculadora.TextChanged += new System.EventHandler(this.Screen_Calculadora_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(257, 269);
+            this.Controls.Add(this.Screen_Calculadora);
+            this.Controls.Add(this.btnCE);
+            this.Controls.Add(this.Btn_Calcular);
+            this.Controls.Add(this.Btn_multiplicacion);
+            this.Controls.Add(this.Btn_resta);
+            this.Controls.Add(this.Btn_suma);
+            this.Controls.Add(this.Btn_num0);
+            this.Controls.Add(this.Btn_num9);
+            this.Controls.Add(this.Btn_num8);
+            this.Controls.Add(this.Btn_num7);
+            this.Controls.Add(this.Btn_num6);
+            this.Controls.Add(this.Btn_num5);
+            this.Controls.Add(this.Btn_num4);
+            this.Controls.Add(this.Btn_num3);
+            this.Controls.Add(this.Btn_num2);
+            this.Controls.Add(this.Btn_num1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_num1;
+        private System.Windows.Forms.Button Btn_num2;
+        private System.Windows.Forms.Button Btn_num3;
+        private System.Windows.Forms.Button Btn_num4;
+        private System.Windows.Forms.Button Btn_num5;
+        private System.Windows.Forms.Button Btn_num6;
+        private System.Windows.Forms.Button Btn_num7;
+        private System.Windows.Forms.Button Btn_num8;
+        private System.Windows.Forms.Button Btn_num9;
+        private System.Windows.Forms.Button Btn_num0;
+        private System.Windows.Forms.Button Btn_suma;
+        private System.Windows.Forms.Button Btn_resta;
+        private System.Windows.Forms.Button Btn_multiplicacion;
+        private System.Windows.Forms.Button Btn_Calcular;
+        private System.Windows.Forms.Button btnCE;
+        private System.Windows.Forms.TextBox Screen_Calculadora;
+
+    }
+}
+
---
+++ FrmCalculadora/FrmCalculadora/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmCalculadora
+{
+    public partial class Form1 : Form
+    {
+        int primer_valor, segundo_valor,resultado;
+        string operacion;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            segundo_valor = Convert.ToInt32(Screen_Calculadora.Text);
+            switch (operacion) {
+                case "+":
+              resultado= primer_valor + segundo_valor;
+              Screen_Calculadora.Text = resultado.ToString();
+                    break;
+
+                case "-":
+              resultado= primer_valor - segundo_valor;
+              Screen_Calculadora.Text = resultado.ToString();
+                    break;
+                case "x":
+              resultado= primer_valor * segundo_valor;
+              Screen_Calculadora.Text = resultado.ToString();
+              break;  
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Screen_Calculadora.Text = Screen_Calculadora.Text + "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Screen_Calculadora.Text = Screen_Calculadora.Text + "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Screen_Calculadora.Text = Screen_Calculadora.Text + "3";
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Screen_Calculadora.Text = Screen_Calculadora.Text + "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Screen_Calculadora.Text = Screen_Calculadora.Text + "5";
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Screen_Calculadora.Text = Screen_Calculadora.Text + "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Screen_Calculadora.Text = Screen_Calculadora.Text + "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Screen_Calculadora.Text = Screen_Calculadora.Text + "8";
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            Screen_Calculadora.Text = Screen_Calculadora.Text + "9";
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Screen_Calculadora.Text = Screen_Calculadora.Text + "0";
+        }
+
+        private void Screen_Calculadora_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCE_Click(object sender, EventArgs e)
+        {
+            Screen_Calculadora.Clear();
+        }
+
+        private void Btn_suma_Click(object sender, EventArgs e)
+        {
+            operacion = "+";
+            primer_valor = Convert.ToInt32(Screen_Calculadora.Text);
+            Screen_Calculadora.Clear();
+        }
+
+        private void Btn_resta_Click(object sender, EventArgs e)
+        {
+            operacion = "-";
+            primer_valor = Convert.ToInt32(Screen_Calculadora.Text);
+            Screen_Calculadora.Clear();
+        }
+
+        private void Btn_multiplicacion_Click(object sender, EventArgs e)
+        {
+            operacion = "x";
+            primer_valor = Convert.ToInt32(Screen_Calculadora.Text);
+            Screen_Calculadora.Clear();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // En propiedades se habilita KeyPreview: True para 
+            //que podamos introducir numeros con el teclado
+            switch (e.KeyChar.ToString())
+            {
+                case "0":
+                    Btn_num0.PerformClick();
+                    break;
+                case "1":
+                    Btn_num1.PerformClick();
+                    break;
+                case "2":
+                    Btn_num2.PerformClick();
+                    break;
+                case "3":
+                    Btn_num3.PerformClick();
+                    break;
+                case "4":
+                    Btn_num4.PerformClick();
+                    break;
+                case "5":
+                    Btn_num5.PerformClick();
+                    break;
+                case "6":
+                    Btn_num6.PerformClick();
+                    break;
+                case "7":
+                    Btn_num7.PerformClick();
+                    break;
+                case "8":
+                    Btn_num8.PerformClick();
+                    break;
+                case "9":
+                    Btn_num9.PerformClick();
+                    break;
+                    // Operaciones:
+                case "+":
+                    Btn_suma.PerformClick();
+                    break;
+                case "*":
+                     Btn_multiplicacion.PerformClick();
+                    break;
+                case "-":
+                    Btn_resta.PerformClick();
+                    break;
+           
+            }
+        }
+
+       
+    }
+}
